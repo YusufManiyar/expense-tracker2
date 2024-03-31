075b0a47079d3426a242cab80ae162c04e0a427d
Tejas150 <tejaskamble1629@gmail.com>
2024-03-31 18:12:50 +0530

    delete file

--- frontend/jwtUtils.js
+++
@@ -1,8 +0,0 @@
-export function decodeJwtToken(token) {
-    const parts = token.split('.');
-    if (parts.length !== 3) {
-        throw new Error('Invalid JWT token format');
-    }
-    const payload = JSON.parse(atob(parts[1]));
-    return payload;
-}
