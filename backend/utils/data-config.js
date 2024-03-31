4f204122b327508e0ddee34bac0889592124568d
Tejas150 <tejaskamble1629@gmail.com>
2024-03-31 18:08:17 +0530

    update db

--- backend/utils/data-config.js
+++ backend/utils/data-config.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense_tracker', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   });
 
